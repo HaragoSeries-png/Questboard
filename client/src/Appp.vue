663213cd77d6f1e3a65e101b4cd96549f3f47b62
o-AI-o <MinaseKoito09@gmail.com>
2020-12-31 16:15:16 +0700

    Config Navbar.vue

--- client/src/Appp.vue
+++ client/src/Appp.vue
@@ -1,40 +1,7 @@
 <template>
-
   <div id="app">
-    <nav>
-        <div class="logo">
-            Quest Board
-        </div>
-        <div class="nav-search">
-      <input type="text" placeholder="Search.." name="search" size="40px" style="font: size 25px;height:25px">
-        <i class="fa fa-search" aria-hidden="true" style="color: white; cursor: pointer; font:size 30px;"></i>
-        </div>
-        <ul class="nav-links" >
-        <li>
-            <a href="#">{{$store.getters.isLoggedIn}}</a>
-        </li>
-        <li>
-            <a href="">About</a>
-        </li>
-        <li>
-            <router-link to="/feed"><a href="#">Work</a></router-link>
-        </li>
-        <li>
-            <router-link to="/login" v-if="!$store.getters.isLoggedIn"><a href="#"> login</a></router-link>
-            <a v-on:click="logout" v-else> logout</a>
-        </li>
-        </ul>
-        <div class="burger" v-on:click="navSlide();">
-            <div class="line1"></div>
-            <div class="line2"></div>
-            <div class="line3"></div>
-        </div>
-        <div class="display_name" v-if=$store.getters.isLoggedIn>        
-            <router-link to="/profile"><a href="#">hee {{$store.getters.getusername}}</a></router-link>
-        </div>
-    </nav>   
+    <Navbar @logout="logout" />
     <router-view></router-view>
-    
   </div>
 </template>
 
@@ -44,214 +11,54 @@
 // import hello from './components/HelloWorld.vue'
 // import VueRouter from 'vue-router'
 // import Vue from 'vue'
+import authService from "@/service/Authservice";
 
-import authService from '@/service/Authservice'
-
-
+import Navbar from "@/components/layout/navbar";
 
 export default {
-  name: 'App',
+  name: "App",
+  components: {
+    Navbar,
+  },
   data() {
     return {
-        islogon:this.$store.getters.isLoggedIn
-    }
+      islogon: this.$store.getters.isLoggedIn,
+    };
   },
-  
+
   methods: {
-    navSlide : () =>{
-        const burger = document.querySelector(".burger");
-        const nav    = document.querySelector(".nav-links");
-        const navLinks = document.querySelectorAll('.nav-links li');
-        burger.addEventListener('click',()=>{
-            nav.classList.toggle('nav-active');
-            navLinks.forEach((link,index)=>{
-                if(link.style.animation){
-                    link.style.animation ='';   
-                }
-                else {
-                    link.style.animation = 'navLinkFade 0.5s ease forwards $(index / 7 + 2s )';
-                    
-                }
-            });
-            burger.classList.toggle('toggle');
-        
+    navSlide: () => {
+      const burger = document.querySelector(".burger");
+      const nav = document.querySelector(".nav-links");
+      const navLinks = document.querySelectorAll(".nav-links li");
+      burger.addEventListener("click", () => {
+        nav.classList.toggle("nav-active");
+        navLinks.forEach((link, index) => {
+          if (link.style.animation) {
+            link.style.animation = "";
+          } else {
+            link.style.animation =
+              "navLinkFade 0.5s ease forwards $(index / 7 + 2s )";
+          }
         });
+        burger.classList.toggle("toggle");
+      });
     },
-    logout(){
-        
-        authService.logout();
-        this.$store.dispatch('deluser').then(this.islogon=false)
-        this.$router.push({path:'/login'})
-    }      
+    logout() {
+      authService.logout();
+      this.$store.dispatch("deluser").then((this.islogon = false));
+      this.$router.push({ path: "/login" });
+    },
+    Polar() {
+        console.log("Can Pass")
+    }
   },
   computed: {
-    out(){
-        return this.islogon
-    }  
-  }
-}
+    out() {
+      return this.islogon;
+    },
+  },
+};
 </script>
-<style scoped >
-    @import 'styles/nav.css';
-</style>
-<style scoped>
 
-
-body{
-margin : 0;
-padding : 0;
-background : url(https://www.facebook.com/NetflixTH/photos/a.292183707902294/1130505247403465/?__cft__[0]=AZX_COK714_cGvilvVwzz2OuKdfemAKSoqPJxHsj5YZMPANJVPbymJDx2S_IvpOU5kjqRNYFylqFO2i0Xbd67OQ_mbWy88hnHVHgTwEV2hNw4you4rY_3EGh8JJrUsAn0DwHpvxs__Qx0CX8urkfpusgjxUlHGQQyvLMBhrSrSDfvjT6SgkiDu0aWkd2f7T-7GfDzfpGVIp6qClmn46Kv7hk&__tn__=EH-y-R);
-background-repeat:no-repeat ;
-background-position: center;
-background-size: auto;
-font-family: sans-serif;
-}
-*{
-    margin:0;
-    padding: 0;
-    box-sizing: border-box;
-}    
-nav{
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    min-height: 8vh;
-    font-family: 'Poppins', sans-serif;
-    background-color: #82b340;
-}
-.logo{
-    color: white;
-}
-.nav-links{
-    display: flex;
-    background-color: #ececec;
-    width: 30%;
-    justify-content: space-around;
-      
-}
-.nav-links a{
-    color: black;
-    text-decoration: none;
-    letter-spacing: 2px;
-    font-weight: bold;
-    font-size: 14px;
-    padding-left: 30px;
-    padding-right: 20px;
-    margin-left: 1px;
-    
-}
-.nav-links li{
-    list-style: none;
-    color: white;
-    align-items: center;
-    padding: 6px;
-    cursor: pointer;
-    border: none;
-}
-/* .nav-links :hover{
-    border: 1px solid black; 
-    background-color: #25383c;
-    transition-property: background-color;
-    transition-duration: 1s;  
-} */
-
-.nav-links li:hover{
-    border: 1px solid black;
-    background-color: #25383c;
-    transition-property: background-color;
-    transition-duration: 1s;  
-}
-.nav-links a:hover{
-    color: #ececec;
-}
-.burger{
-    display: none;
-    border: solid 1px white;
-}
-
-.burger div{
-    background-color: white;
-    width: 25px;
-    height: 4px;
-    margin: 5px;
-    transition: all 0.5s ease;
-}
-.nav-search{
-    
-    position: relative;
-    padding-left: 0.5%;
-    margin-right:20%
-    
-}
-@media screen and (max-width:1024px){
-.nav-links{
-    width: 60%;
-    } 
-}
-@media screen and (max-width:768px){
-    body{
-        overflow-x:hidden;
-        width: 100%
-    }
-.nav-links{
-    position: absolute;
-    right: 0px;
-    height: 52vh;
-    top: 8vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 50%;
-    transform: translateX(100%);
-    transition: transform 0.5s ease-in;
-    cursor: pointer;
-    opacity: 0;
-    } 
-
-    .nav-links li{
-        opacity: 1;
-    }
-    .burger{
-        display: block;
-    }
-.nav-active{
-    transform: translateX(0%);
-    transition: transform 0.5s ease-in;
-    opacity: 1;
-}
-@keyframes navLinkFade {
-    from{
-        opacity: 0;
-        transform: translateX(50px);
-    }
-    to{
-        opacity: 1;
-        transform: translate(0px);
-    }
-}
-nav-search-active{
-    transform: translateX(100%);
-    background-color: black;
-}
-
-.toggle .line1{
-    transform: rotate(-45deg) translate(-5px,6px);
-}
-.toggle .line2{
-    opacity: 0;
-}
-.toggle .line3{
-    transform: rotate(45deg) translate(-5px,-6px);
-}
-
-}
-@media screen and (max-width:1200px){
-.nav-search{
-    display: none;
-}
-.display_name{
-    display: none;
-}
-}
-
-</style>
+<style></style>
--- server/index.js
+++ server/index.js
@@ -1,18 +1,16 @@
-const   express = require('express'),
-        bodyParser = require('body-parser'),
-        cors= require('cors'),
-        mongoose = require('mongoose'),
-        passport =  require('passport');
-        require('./configs/passport'),
-        passportLocalMongoose = require('passport-local-mongoose'),
-        passportLocal = require('passport-local'),
-        User = require('./models/user.model'),
-        methodOverride = require("method-override")
-        
-        
+const express = require('express'),
+    bodyParser = require('body-parser'),
+    cors = require('cors'),
+    mongoose = require('mongoose'),
+    passport = require('passport');
+require('./configs/passport'),
+    passportLocalMongoose = require('passport-local-mongoose'),
+    passportLocal = require('passport-local'),
+    User = require('./models/user.model'),
+    methodOverride = require("method-override")
 
 const app = express();
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(require('express-session')({
     secret: 'Qusetboard',
     resave: false,
@@ -25,23 +23,21 @@ app.use(methodOverride("_method"));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
-mongoose.connect("mongodb+srv://chanon:132231@cluster0-broqy.mongodb.net/Questboard?retryWrites=true&w=majority", {useNewUrlParser:true})
-.then(()=>{
-    console.log('successfully');
-})
-.catch(e=>console.log('error'))
+mongoose.connect("mongodb+srv://chanon:132231@cluster0-broqy.mongodb.net/Questboard?retryWrites=true&w=majority", { useNewUrlParser: true })
+    .then(() => {
+        console.log('successfully');
+    })
+    .catch(e => console.log('error'))
 
 app.use(bodyParser.json())
 app.use(cors());
 
-
 const posts = require('./routes/api/posts');
 const auth = require('./routes/api/auth');
 const profile = require('./routes/api/profile');
-app.use('/api/posts',posts);
-app.use('/api/auth',auth);
-app.use('/api/profile',profile);
+app.use('/api/posts', posts);
+app.use('/api/auth', auth);
+app.use('/api/profile', profile);
 const port = process.env.PORT || 5000
 
-app.listen(port,()=>console.log('server start at '+port))
+app.listen(port, () => console.log('server start at ' + port))
