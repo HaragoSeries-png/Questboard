8508409c55c813a7d741757a606224bc17bf9212
junior <jiraphat-saeheng@hotmail.com>
2021-01-07 00:45:38 +0700

    createquest2

--- client/src/router/AppControl.js
+++ client/src/router/AppControl.js
@@ -3,6 +3,7 @@ import profile from '../views/profile.vue'
 import signup from '../views/signup.vue'
 import questfeed from '@/views/questfeed'
 import createQuest from '../views/createQuest.vue'
+import createQuest2 from '../views/createQuest2.vue'
 import VueRouter from 'vue-router'
 import Vue from 'vue'
 
@@ -12,7 +13,8 @@ const routes = [
     { path: '/profile', component: profile },
     { path: '/signup', component: signup },
     { path:'/feed',component:questfeed},
-    {path: '/createQuest',component:createQuest}
+    {path: '/createQuest',component:createQuest},
+    {path: '/createQuest2',component:createQuest2}
 ]
 const router = new VueRouter({mode: 'history',routes})
 
---
+++ client/src/views/createQuest2.vue
@@ -0,0 +1,299 @@
+<template>
+
+  <div>
+    <v-container style=" width:60%; border:1px solid black; margin-top:5%; height:100%;" >
+        <!-- <div class="main"> -->
+
+      <v-row>
+        <v-col cols="12" md="12" sm="12" class="section1">
+
+          <center>
+            <h2 id="head">WANTED</h2>
+            <div class="pictureQuest">
+              <img v-bind:src="Pic1" />
+            </div>
+          </center>
+            <center>
+          <div class="text-fill">
+            <div>
+              <table>
+                <tr>
+                  <span id="Deadline"> Deadline </span>
+                  <div><i class="material-icons">assessment</i></div>
+                  <td></td>
+                </tr>
+                <tr>
+                  <span id="Date"> Date : </span>
+                  <td>
+                    <input type="date" name="" id="I_date" />
+                  </td>
+                </tr>
+                <tr>
+                  <span id="Start"> Start : </span>
+                  <td>
+                    <input type="time" name="" id="I_start" />
+                  </td>
+                </tr>
+                <tr>
+                  <span id="End"> End : </span>
+                  <td>
+                    <input type="time" name="" id="I_end" />
+                  </td>
+                </tr>
+                <tr id="main_Person">
+                  <i class="material-icons">person</i>
+                  <td>
+                    <input
+                      type="number"
+                      min="0"
+                      name=""
+                      id="num_per"
+                      v-model="numberofcon"
+                    />
+                  </td>
+                </tr>
+                  <tr>
+              <span id="q1"> Quest name : </span>
+              <td>
+                <v-text-field
+                style="margin-bottom:5%;"
+                    id="iq"
+                  :rules="rules"
+                  counter="25"
+                  hint="Baby sister,Tutor "
+                  label="Quest"
+                ></v-text-field>
+              </td>
+              
+            </tr>
+            <tr>
+              <span id="c1"> Category : </span>
+              <td>
+                <v-select :items="items" id="ic" label="Default" style="margin-bottom:5%;" dense></v-select>
+              </td>
+            </tr>
+            <tr>
+              <span id="d1">Details :</span>
+              <td >
+                <v-textarea
+                  v-model="title"
+                  label="More information about quest"
+                  counter
+                  maxlength="120"
+                 
+                  filled
+                  id="id"
+                ></v-textarea>
+              </td>
+            </tr>
+               </table>
+
+        
+             <center><v-btn id="btn"
+              x-medium
+              color="success"
+            >
+              Create
+              </v-btn></center>
+
+
+
+
+                
+           
+
+             
+            </div>
+          </div>
+          </center>
+        </v-col>
+
+    
+            <!-- <tr>
+              <span id="q1"> Quest name : </span>
+              <td>
+                <v-text-field
+                style="margin-bottom:5%;"
+                    id="iq"
+                  :rules="rules"
+                  counter="25"
+                  hint="Baby sister,Tutor "
+                  label="Quest"
+                ></v-text-field>
+              </td>
+              
+            </tr>
+            <tr>
+              <span id="c1"> Category : </span>
+              <td>
+                <v-select :items="items" id="ic" label="Default" style="margin-bottom:5%;" dense></v-select>
+              </td>
+            </tr>
+            <tr>
+              <span id="d1">Details :</span>
+              <td>
+                <v-textarea
+                  v-model="title"
+                  label="More information about quest"
+                  counter
+                  maxlength="120"
+                  full-width
+                  single-line
+                  filled
+                  id="id"
+                ></v-textarea>
+              </td>
+            </tr>
+          
+             <center><v-btn id="btn"
+              x-large
+              color="success"
+            >
+              Create
+              </v-btn></center> -->
+            
+        
+        
+      </v-row>
+      <!-- </div> -->
+    </v-container>
+        <center><div id="foot">Quest Board</div></center>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  name: "createQuest2",
+  data() {
+    return {
+      items: ["Crafter", "House worker", "Handicup", "Etc"],
+      Pic1: "https://pbs.twimg.com/media/EBBMoBNU4AA2DXn.jpg",
+    };
+  },
+};
+</script>
+
+<style scoped>
+.pictureQuest img {
+  width: 400px;
+  height: 200px;
+  margin-top: 2vh;
+  justify-content: center;
+}
+.pictureQuest {
+  justify-items: center;
+}
+.text-fill {
+  margin-top: 5%;
+}
+.text-fill table {
+  margin-left: auto;
+  margin-right: auto;
+}
+.text-fill table tr,
+td {
+  margin-top: 20%;
+}
+/* .text-fill table td,tr{
+    border: 1px solid black;
+} */
+.v-text-field {
+  margin-left: 4%;
+  width: 200px;
+  height: 50px;
+  font: 20px;
+}
+.section2{
+    margin-top: 10%;
+}
+.v-textarea {
+  width: 400px;
+  height:200px
+}
+.v-text-field input {
+  font-size: 80px;
+}
+#reward {
+  margin-bottom: 20px;
+  font-size: 18px;
+}
+#Deadline {
+  font-size: 18px;
+  padding-top: 30px;
+}
+.main{
+    width: 100%;
+    align-content: center;
+    border-left: 1px solid black;
+    border-right: 1px solid black;
+}
+#I_date {
+  margin-left: 7px;
+  border: 1px solid black;
+  margin-bottom: 10px;
+}
+#Start {
+  font-size: 18px;
+}
+#End {
+  font-size: 18px;
+}
+#foot {
+  font-family: "Redressed", cursive;
+  font-size: 20px;
+  margin-top: 1%;
+}
+#Date {
+  font-size: 18px;
+}
+#head {
+  text-decoration: underline;
+  font-family: "Roboto Mono", monospace;
+  letter-spacing: 1px;
+  font-size: 30px;
+}
+#I_start {
+  margin-left: 7px;
+  margin-top: 5px;
+  border: 1px solid black;
+  border-radius: 8%;
+  margin-bottom: 10px;
+}
+#I_end {
+  margin-left: 7px;
+  margin-top: 5px;
+  border: 1px solid black;
+  border-radius: 8%;
+  margin-bottom: 10px;
+}
+/* .text-fill table tr,td{
+    border: 1px solid black;
+} */
+#num_per {
+  margin-top: 5px;
+  margin-left: 7px;
+  border: 1px solid black;
+  width: 50px;
+  padding-left: 10px;
+   margin-bottom: 10px;
+}
+#q1{
+    font-size: 18px;
+    
+}
+#c1{
+    font-size: 18px;
+}
+#d1{
+    font-size: 18px;
+}
+
+#btn{
+    margin-top: 10%;
+}
+#iq{
+    margin-bottom: 5;
+}
+
+</style>
