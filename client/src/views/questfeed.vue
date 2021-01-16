3b707e5325a25e21e9092d69d4b5c46730385da6
Siraw1998 <SirawNorth@gmail.com>
2021-01-16 14:10:21 +0700

    Faith#2

--- client/src/views/questfeed.vue
+++ client/src/views/questfeed.vue
@@ -1,118 +1,162 @@
+
 <template>
-  <div id="questfeed">
-    <v-form>
-      <v-container>
-        <v-row>
-          <v-col cols="12" md="4">
-            <div style="display: none;">
-              <v-file-input
-                truncate-length="15"
-                label="image"
-                width="50%"
-                v-model="files"
-                @change="onFileChange"
-                id="fileUpload"
-              ></v-file-input>
-            </div>
-          </v-col>
-          <v-spacer></v-spacer>
-
-          <v-btn @click="sendim">
-            submit
-          </v-btn>
-          <v-btn @click="getinfoma">profile</v-btn>
-        </v-row>
-        <v-row> </v-row>
-      </v-container>
-    </v-form>
-
-    <v-img
-      max-height="200"
-      max-width="200"
-      src="http://localhost:5000/2021-01-06T12-45-36.660Zmiku.gif"
-      @click="chooseFiles()"
-      >hee</v-img
-    >
-
-    <div id="preview">
-      <v-img v-if="url" :src="url" max-height="250" max-width="250"></v-img>
+<div>
+    <div class="category">
+       <a href="./">Handicraft</a> 
+        <a href="./">Advice</a>
+        <a href="./">Education</a>
+        <a href="./">Accident</a>
+        <a href="./">Housework</a>
+        <a href="./">Find friends</a>
+        <a href="./">Food and home economics</a>
+        <a href="./">Traffic</a>
     </div>
-    <v-dialog v-model="dialog" width="500">
-      <template v-slot:activator="{ on }">
-        <v-btn class="success" v-on="on">Add New Project</v-btn>
-      </template>
-      <v-card>
-        <v-card-title class="headline grey lighten-2" primary-title
-          >Add a New Project</v-card-title
-        >
-      </v-card>
-    </v-dialog>
-  </div>
-</template>
+    
+    <ul>
+        <li v-for="(item,index) in quests" :key="index"><Questcard></Questcard></li>
+    </ul>
+       
 
+ <div class="page">
+            <div class="bar">
+              <a href="#" class="button">«</a>
+              <a href="#" class="button">1</a>
+              <a href="#" class="button">2</a>
+              <a href="#" class="button">3</a>
+              <a href="#" class="button">4</a>
+              <a href="#" class="button">»</a>
+            </div>
+ </div>
+ </div>
+</template>>
+<style>
+        body{background-image: url();
+            background-attachment: fixed;
+            background-size: cover;
+        }
+        .cards{
+            margin: 0 auto;
+            max-width: 1300px;
+            display: grid;
+            grid-template-columns: repeat(5,1fr);
+            gap: 30px;   
+            font-family: sans-serif;
+           
+        }
+        .card{
+           
+            box-shadow: 0 0 5px rgb(0, 0, 0);
+           
+        }
+        .card__img{
+            width: 100%;
+            display: block;
+        }
+        .card__content{
+            line-height: 1.5;
+            font-size: 0.9em;
+            padding: 15px;
+            background: #fafafa;
+        }
+        .card__content>h2:first-of-type{
+            margin-top: 0;
+        }
+        .card__content > p:first-of-type{
+            margin-top: 0;
+        }
+        .card__content>h2:last-of-type{
+            margin-bottom: 0;
+        }
+        .card__info{
+            padding: 15px;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            background: #eeeeee;
+            font-size: 0.8em;
+            border-bottom: 2px solid #cccccc;
+        }
+    .more{
+        color: #6a966a;
+        text-decoration: none;
+    }
+    .more:hover{
+        text-decoration: underline;
+    }
+    .category{
+        font-size: 25px;
+        padding: 50px;
+        text-align: center
+    }
+    .category a{
+        color: #072448;
+        background: #f8aa4b;
+        padding: 15px;
+        text-decoration: none;
+        border-radius: 40px;
+        font-family: sans-serif;
+        box-shadow: 0 2px 5px  #54d2d2
+    }
+    .category a:hover{
+        color: #ffa822;
+        background: #134e6f;
+        box-shadow: 5px 0 10px #54d2d2;
+        text-decoration: underline;
+    }
+    .page{
+        font-family: sans-serif;
+        font-size: 30px;
+        text-align: center;
+        padding: 50px;
+        
+    }
+    .page .button{
+        color: #072448;
+        padding: 10px;
+        text-decoration: none;
+        text-shadow:0 2px 5px #f8aa4b ;
+    }
+    .page .button:hover{
+        color: #f8aa4b;
+        font-size: 50px;
+        text-decoration: underline;
+    }
+    @media screen and (max-width: 768px) {
+    .category a{
+    font-size: 10px;
+    padding: 10px;
+  }
+  .cards{
+            grid-template-columns: repeat(2,1fr);
+}}
+@media screen and (max-width: 1024px) {
+    .category a{
+    font-size: 10px;
+    padding: 10px;
+  }}
+    </style>
 <script>
-import questService from "../service/Questservice";
-import profileService from "../service/profileservice";
-
+import Questcard from '../components/Questcard'
+import Questservice from '../service/Questservice'
 export default {
-  name: "Quest Feed",
-  methods: {
-    add() {
-      this.$store.dispatch("set");
+    components:{
+        Questcard
     },
-    sendim: async function() {
-      if (this.files) {
-        alert("inn");
-        let formData = new FormData();
-
-        // files
-        formData.append("image", this.files);
-
-        // additional data
-        formData.append("questname", "foo bar");
-
-        let suc = await questService.createquest(formData).then((res) => {
+name: "questsfeed",
+methods:{
+    getquest: async function(){
+       let a = await Questservice.getquest().then((res) => {
           return res;
         });
-        console.log("logsuc" + suc);
-
-        if (suc) this.$router.push({ path: "/login" });
-        else alert("fail");
-      } else console.log("there are no files.");
-    },
-    onFileChange() {
-      if (this.files != null) {
-        const file = this.files;
-        console.log(file);
-        this.url = URL.createObjectURL(file);
-      }
-    },
-    chooseFiles() {
-      document.getElementById("fileUpload").click();
-    },
-    getinfoma: async function() {
-      console.log("gett");
-      let re = await profileService.getprofile().then((res) => {
-        return res;
-      });
-      console.log("dadsaaaaaaa" + re);
-      if (re.suc) {
-        console.log(re.infoma);
-        alert("yeah");
-      } else alert("fail");
-    },
-  },
-  created() {
-    this.$emit("setTitle", this.$options.name);
-  },
-  data() {
+        console.log(a.suc)
+    }
+},
+ data() {
     return {
-      count: this.$store.getters.getcount,
-      files: null,
-      url: null,
-    };
-  },
-};
-</script>
+    quests:[]
+    }
+  }   
+}
+  
 
-<style scoped></style>
+</script>
