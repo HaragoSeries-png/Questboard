e604cba6e1b9b97b2eb8f526f039251dc61f2740
junior <jiraphat-saeheng@hotmail.com>
2021-02-19 13:25:28 +0700

    ..

--- admin/src/components/questbox.vue
+++ admin/src/components/questbox.vue
@@ -1,7 +1,9 @@
 <template>
 <div id="box">
+
+
     <div>
-        name:{{this.name}}
+        name:{{name}}      
     </div>
     
    
@@ -28,6 +30,8 @@
               </div></li>
         </ul>
       </template>
+
+
       <v-card>
         <v-card-title class="headline">
           Use Google's location service?
@@ -69,7 +73,6 @@ import adminService from '../../service/adminService'
 export default {
     props:['name','qstatus','qid'],
     methods:{
-        
         reject(){
           adminService.decide(this.qid,false,this.rating)  
           this.$emit("reload")  
@@ -85,7 +88,8 @@ export default {
       return{
         dialog: false,
         rating:0,
-        status:false
+        status:false,
+        
       }
     }
        
